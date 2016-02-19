3dff8c003e8b08103ebfff51923a139e94f09ae0
Iman <iman_avaz@hotmail.com>
2016-02-19 23:07:23 +1100

    added file read functionality.

---
+++ iDIGit/iDIGit/Scripts/fileProcessing.js
@@ -0,0 +1,48 @@
+﻿var contents;
+
+
+function readSingleFile(eve) {
+
+    var file = eve.target.files[0];
+
+    if (!file) {
+        return;
+    }
+
+    var parts = file.name.split('.');
+    var fileExt = parts[parts.length - 1];
+        
+    switch (fileExt.toLowerCase()) {
+        case ('csv'):
+            processCSV(file);
+            return;
+        case ('txt'):
+            processTXT(file);
+            return;
+    }
+    
+}
+
+function processCSV(f)
+{
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        contents = e.target.result;
+        console.log(contents);
+    };
+
+    reader.readAsText(f);
+}
+
+function processTXT(f)
+{
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        contents = e.target.result;
+        console.log(contents);
+    };
+
+    reader.readAsText(f);
+}
